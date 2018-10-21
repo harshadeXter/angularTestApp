de9f43abbc1c0b64d2abfe396c90b0e2c36b3146
harshadeXter <hlpathum@gmail.com>
2018-10-21 20:01:04 +0530

    Add more routing to the application.

--- HeroApplication/src/app/app.module.ts
+++ HeroApplication/src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailsComponent } from './hero-details/hero-details.component';
-import { MessageComponent } from './message/message.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailsComponent }  from './hero-details/hero-details.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { MessageComponent }    from './message/message.component';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailsComponent,
-    MessageComponent,
-    DashboardComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailsComponent,
+    MessageComponent
   ],
   bootstrap: [ AppComponent ]
 })
